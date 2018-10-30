8b01d6720d859d7987d0c560ec20b51af7431bdd
JavierJRRamirez <Javier.Ramirez@colorado.edu>
2018-10-30 11:50:27 -0600

    VideoLink

--- FlightService/src/com/csci4448/project/controller/AdminController.java
+++ FlightService/src/com/csci4448/project/controller/AdminController.java
@@ -1,5 +1,11 @@
 package com.csci4448.project.controller;
 
 public class AdminController {
+	public static void checkVacency(int flightNumber){
+		
+	}
 	
+	public static void searchClient(String firstName){
+		
+	}
 }
